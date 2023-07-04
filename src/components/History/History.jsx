f5d618c4ed520552e3255ef336c383f6c862ae6c
natolay <ndixon@seattleu.edu>
2023-07-04 15:13:21 +0200

    updated history page

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -6,84 +6,99 @@ const History = () => {
     return(
 <>
 <div>
-    <div> Bygdegårdens historia</div>
+    <div className="historyheader"> Bygdegårdens historia</div>
 </div>
 
 
-<div> 
+<section className="historytext"> 
 <div>
 Bygdegårdsföreningen bildades 20 januari 1945 på initiativ av JUF. 1949 uppfördes 
 en militärbarack med bänkar, bord, porslin, köksutrustning och uthus med dass. Den totala kostnaden 
 för detta var 9000kr! Invigningen skedde 12-13 november samma år, kykoherde Sten Östergren var invigningstalare.
+</div>
 
+<div>
 Redan på årsmötet året efter uppmanades man komma med förslag på tillbyggnad av bygdegården. Fem 
 år senare, 1955, skedde en stor om- och tillbyggnad, i stort sett till dagens omfattning. Detta arbete leddes 
 av K.G. Lindelöf. Finansieringen av ombyggnaden löstes dels genom lån i Sparbanken och dels genom att ordföranden 
 lyckats utverka ett lån av en äldre förmögen dam mot revers med 3% ränta. Såvitt kunnat utredas är det tveksamt om 
 detta lån återbetalats! Man anordnade också ett mopedlotteri men lotterna var svårsålda, dragningen fick flyttas fram 
 en månad.
-
+</div>
+<div>
 Tage Lundmark, som varit ordförande sedan föreningen startats, utsågs också till kassör 
 och lokaluthyrare vid årsmötet 1951. Hans arvode var hela 30kr per år.
-
+</div>
+<div>
 Lundby har under årens lopp varit föregångare i många sammanhang:
 Från 1956 och ett tiotal år framåt anordnades ett familjerally där över 80 ekipage deltog.
 1959 avslutades årsmötet med att man såg kvällens TV-program, efter detta följde ett antal TV-kvällar.
 I början av 60-talet anordnades frågesport. Lag som deltog i tävlingen 1961 
 var: RLF, 4-H, JUF, SLKF, Syföreningen, RK, Fruktodlarföreningen, Bordtennisklubben, Skytteföreningen 
 och Bygdegårdsföreningen.
-
+</div>
+<div>
 1964 genomfördes bygget av en toalett, man hade en stor högtidlig invigning.
-
+</div>
+<div>
 Den 4 april 1970 hade man ett dramatiskt årsmöte. Efter festligheter med mat, dryck och 
 underhållning avslutades det hela nämligen med en oförglömlig snöstorm. Flertalet besökare var i stor 
 fara på väg hem.
-
+</div>
+<div>
 Uppfinningsrikedomen för att underhålla vid olika festligheter har varit minst 
 sagt fantasifull. Den har förekommit allt från tävlan om vem som kunnat visa upp de skitigaste 
 fötterna till att styrelsefruarna fått välja ut sin respektive genom att enbart få se bara ben och fötter.
-
+</div>
+<div>
 Under årsmötet 1977 utmanade Gösta Gruvaeus, innehavare av en tioårig
  titel som miss Lundby, de andra i styrelsen. Efter en jämn och hård kamp vann 
  Hans Jonsson titeln som ny miss Lundby!
-
+ </div>
+ <div>
 1980 inleddes en ny utveckling av Bygdegården. Då anlades en provisorisk utedansbana för sommarfester 
 och kräftskivor, dessa tillställningar blev så välbesökta att de gav ekonomiska möjligheter att året efter 
 anlägga en permanent bana.
-
+</div>
+<div>
 Vid denna tid debuterade en ny form av underhållning, kåserier benämnda Älgnytt. Här har behandlats inte bara 
 rena älgföreteelser utan också andra allmänt intessanta händelser.
-
+</div>
+<div>
 1992 var det premiär för PUB och Lundby var som vanligt först när det gäller nytänkande. Ett annat
  exempel på detta är Traktorracet som startades 1995 och har blivit en succé såväl publikt som ekonomiskt.
-
+ </div>
+ <div>
 Under 90-talet har en ombildning från ekonomisk till ideéll förening genomförts. En altan och en handikappanpassad 
 tillbyggnad har uppförts plus omfattande arbete med utbygge och tak till dansbanan.
-
+</div>
+<div>
 <i>Det är på grund av fantasi och goda arbetsinsatser från många som Bygdegården står rustad 
 som en fortsatt attraktiv tillgång. Dock är det några som ändå betytt särskilt mycket för föreningen. 
 Främst den som ledde uppbyggnaden av föreningen, Tage Lundmark. Därutöver kan nämnas Gösta Gustavsson, Assar Wedner
  och Anders Ekblom som varit ordförande i styrelsen. Någon som också bör uppmärksammas är Ragnar Ljungberg som var
   ledamot i styrelsen längre än någon annan, nämligen under 30 år och av dessa var han kassör i 25 år.</i>
 </div>
-</div>
+</section>
 
 <div>
-    <div>
-    Milstolpar:
-20 januari 1945: Bygdegårdsföreningen bildas
- 1949: Första "byggnaden"- en militärbarack uppförs
-  1955: En stor om- och tillbyggnad genomförs
-  1956: och 10 år framåt anordnas Familjerally
-  1959: Årsmötet avslutas med en TV-kväll. Det kom att bli många fler...
-  1964: Toalett byggdes
-   1980: En provisorisk utedansbana anlades
-   1992: PUBEN gjorde premiär
-   1995: Första Traktorracet
-   2009: En större renovering genomfördes och var klart till 60- årsjubiléet i november. "Bygdegår´n 60 år" Melodi 34:an. Text: Karin Hansson. Läs texten HÄR.
-    2014: Traktorracet firade 20- års jubileum
- 2015: Föreningens 70- års jubileum
-    </div>
+    <section className="milestones">
+        <div className="milesheader"><strong>Milstolpar:</strong></div>
+    <div className="milestext">
+<strong>20 januari 1945:</strong> Bygdegårdsföreningen bildas  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; 
+<strong>1949:</strong> Första "byggnaden"- en militärbarack uppförs  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+ <strong> 1955:</strong> En stor om- och tillbyggnad genomförs  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+  <strong>1956:</strong> och 10 år framåt anordnas Familjerally  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+  <strong>1959:</strong> Årsmötet avslutas med en TV-kväll. Det kom att bli många fler...  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+  <strong>1964:</strong> Toalett byggdes  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+   <strong>1980:</strong> En provisorisk utedansbana anlades  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+   <strong>1992:</strong> PUBEN gjorde premiär  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+   <strong>1995:</strong> Första Traktorracet  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+   <strong>2009:</strong> En större renovering genomfördes och var klart till 60- årsjubiléet i november. "Bygdegår´n 60 år" Melodi 34:an. Text: Karin Hansson. Läs texten HÄR.  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+   <strong> 2014:</strong> Traktorracet firade 20- års jubileum  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+ <strong>2015:</strong> Föreningens 70- års jubileum  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+ </div>
+    </section>
 </div>
 </>
     )
